f3487d12c1fc4c54a34ab692c1c7b9bf59b676f4
xuzhengbing322 <2079880383@qq.com>
2022-03-09 17:17:50 +0800

    第九版：重构了文件的目录，优化了gulp代码

--- biliWebCopyT5/gulpfile.js
+++ biliWebCopyT5/gulpfile.js
@@ -1,22 +1,34 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
 const csso = require('gulp-csso')
-const ts = require('gulp-typescript')
+const typescript = require('gulp-typescript')
 
-gulp.task('copy', () => {
-  gulp.src('./src/index.html')
-    .pipe(gulp.dest('dist'))
+gulp.task('copyhtml', () => {
+  return gulp
+    .src('./src/html/index.html')
+    .pipe(gulp.dest('./dist/html'))
 })
 
 gulp.task('cssmin', () => {
-  gulp.src('./src/biliWebCode.less')
+  return gulp
+    .src('./src/less/biliWebCode.less')
     .pipe(less())
     .pipe(csso())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./dist/css'))
 })
 
-gulp.task('default', () => {
-  gulp.src('./src/**/*.ts')
-    .pipe(ts())
-    .pipe(gulp.dest('dist'))
+gulp.task('type', () => {
+  return gulp
+    .src('./src/ts/biliWebCode.ts')
+    .pipe(typescript({
+      noImplicitAny: true,
+      outFile: 'biliWebCode.js'
+    }))
+    .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('images', () => {
+  return gulp
+    .src('./src/images/**')
+    .pipe(gulp.dest('./dist/images'))
 })
